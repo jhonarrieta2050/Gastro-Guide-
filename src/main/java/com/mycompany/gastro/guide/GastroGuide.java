6459d6ab6dbafe9bc08cf2b2bab98994cdeefa53
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-04-13 22:03:44 -0500

    Mi primer commit

---
+++ src/main/java/com/mycompany/gastro/guide/GastroGuide.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gastro.guide;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GastroGuide {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("HOLA");
+        
+        String macaco;
+    }
+}
