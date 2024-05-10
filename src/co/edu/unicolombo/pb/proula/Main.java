32eac3667a514c592a0f88174130d4813d06ec96
Jhon Deivis <jhonarrieta2050@gmail.com>
2024-05-10 15:21:11 -0500

    nuevo repo

---
+++ src/co/edu/unicolombo/pb/proula/Main.java
@@ -0,0 +1,13 @@
+package co.edu.unicolombo.pb.proula;
+
+import co.edu.unicolombo.pb.proula.gui.Loggin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Loggin loggin = new Loggin();
+        loggin.setVisible(true);
+        loggin.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ src/co/edu/unicolombo/pb/proula/gui/PasswordRecover.java
@@ -0,0 +1,198 @@
+
+package co.edu.unicolombo.pb.proula.gui;
+
+import co.edu.unicolombo.pb.proula.gui.logica.Controlador;
+import javax.swing.JOptionPane;
+
+
+public class PasswordRecover extends javax.swing.JFrame {
+
+    private Controlador control;
+    private Loggin loggin;
+    private String usuarioRecover;
+    
+    public void setControl(Controlador control){
+        
+        this.control = control;
+    }
+    
+    public void setLoggin(Loggin loggin){
+        
+       this.loggin  = loggin;
+    }
+    
+    public PasswordRecover() {
+        initComponents();
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        tabbedPane = new javax.swing.JTabbedPane();
+        jPanel3 = new javax.swing.JPanel();
+        jButton2 = new javax.swing.JButton();
+        contrasenaText = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        contrasenaTextP = new javax.swing.JPasswordField();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        nombreField = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel3.setBackground(new java.awt.Color(0, 204, 204));
+
+        jButton2.setText("tab1");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        contrasenaText.setText("Contrasena");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel3.setText("Cambiar contrasena");
+
+        contrasenaTextP.setText("jPasswordField1");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(contrasenaText)
+                            .addComponent(jButton2)
+                            .addComponent(contrasenaTextP, javax.swing.GroupLayout.DEFAULT_SIZE, 158, Short.MAX_VALUE)))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGap(150, 150, 150)
+                        .addComponent(jLabel3)))
+                .addContainerGap(202, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 214, Short.MAX_VALUE)
+                .addComponent(contrasenaText, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(contrasenaTextP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(67, 67, 67)
+                .addComponent(jButton2)
+                .addGap(56, 56, 56))
+        );
+
+        tabbedPane.addTab("tab2", jPanel3);
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setForeground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
+        jLabel1.setText("Olvido su contrasena?");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, -1, -1));
+        jPanel2.add(nombreField, new org.netbeans.lib.awtextra.AbsoluteConstraints(36, 125, 120, -1));
+
+        jLabel2.setText("Usuario");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(36, 103, -1, -1));
+
+        jButton1.setText("Confirmar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(36, 165, -1, -1));
+
+        tabbedPane.addTab("tab1", jPanel2);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(tabbedPane)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(tabbedPane, javax.swing.GroupLayout.PREFERRED_SIZE, 568, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        String nombre = nombreField.getText();
+        
+        if(control.validarNombre(nombre)){
+            JOptionPane.showMessageDialog(this, "No se a encontrado a el usuario", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        nombreField.setText("");
+        
+        this.usuarioRecover = nombre;
+        
+        tabbedPane.setSelectedIndex(1);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+        String contrasena = contrasenaText.getText();
+        char[] charC = contrasenaTextP.getPassword();
+        String contrasenaP = new String(charC);
+        
+        try{
+            
+            if(control.CompararContrasena(contrasena,contrasenaP,usuarioRecover)){
+                
+                JOptionPane.showMessageDialog(null, "Su contrasena a sido cambiada con exito");
+                tabbedPane.setSelectedIndex(0);
+                contrasenaText.setText("");
+                contrasenaTextP.setText("");
+                
+                this.setVisible(false);
+        
+                loggin.setVisible(true);
+                loggin.setLocationRelativeTo(null);
+            }
+            
+        }catch(IllegalArgumentException e){
+            
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            
+        }
+        
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField contrasenaText;
+    private javax.swing.JPasswordField contrasenaTextP;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JTextField nombreField;
+    private javax.swing.JTabbedPane tabbedPane;
+    // End of variables declaration//GEN-END:variables
+}
